521c9dfde63db425be1d4bac67b37769a63307be
“Adam00322” <emailjqh@163.com>
2023-05-30 14:34:29 +0800

    feat: add Api
    Signed-off-by: “Adam00322” <“emailjqh@163.com”>

---
+++ frontend/my_app/components/Api.tsx
@@ -0,0 +1,69 @@
+const rootUrl = 'https://localhost:3000/api';
+
+async function getData(url: string, token = ""): Promise<any> {
+  url = rootUrl + url;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if(token !== "") {
+    myHeaders.append("Authorization", "Bearer " + token);
+  }
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: myHeaders
+  });
+  return response.json();
+}
+
+async function postData(url: string, body: any, token = ""): Promise<any> {
+  url = rootUrl + url;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if(token !== "") {
+    myHeaders.append("Authorization", "Bearer " + token);
+  }
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify(body)
+  });
+  return response.json();
+}
+
+async function putData(url: string, body: any, token = ""): Promise<any> {
+  url = rootUrl + url;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if(token !== "") {
+    myHeaders.append("Authorization", "Bearer " + token);
+  }
+
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: myHeaders,
+    body: JSON.stringify(body)
+  });
+  return response.json();
+}
+
+async function deleteData(url: string, token = ""): Promise<any> {
+  url = rootUrl + url;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if(token !== "") {
+    myHeaders.append("Authorization", "Bearer " + token);
+  }
+
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: myHeaders
+  });
+  return response.json();
+}
+
+export { getData, postData, putData, deleteData };
